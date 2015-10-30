73069fe830573b5806d86d4972311b69706412c7
Jacques Sauve <Jacques@MacBook-Air-de-Jacques.local>
2015-10-30 14:31:31 -0300

    cleanup

--- app.py
+++ app.py
@@ -53,7 +53,34 @@ def get_data(data_file=DATA_FILE):
     return (df, df_volume, df_by_location)
 
 # todo
-# cleanup, github, README, error
+# cleanup, github, README
+TOOLS = "pan,wheel_zoom,box_zoom,reset,resize"
+"""Default bokeh tools to show in graph
+"""
+
+def volumeGraph(df_volume, tools=TOOLS):
+    dates = df_volume.index.map(lambda x: datetime.strptime(x, '%Y-%m-%d'))
+    fig = figure(tools=tools, 
+            title='Posts Volume',
+            x_axis_label='Date',
+            y_axis_label='Volume of Posts',
+            x_axis_type='datetime')
+    fig.line(dates, df_volume.values.tolist(), line_width=3)
+    return components(fig, INLINE)
+
+MAX_NEIGHBORHOODS = 20
+"""Default maximum number of neighborhoods to show in graph
+"""
+
+def locationGraph(df_by_location, max_neighborhoods=MAX_NEIGHBORHOODS):
+    data = {"y": df_by_location.values.tolist()[:max_neighborhoods]}
+    fig = Bar(data, 
+        cat=df_by_location.index.values.tolist()[:max_neighborhoods], 
+        title="Post Volume per Neighborhood",
+        xlabel='Neighborhood', 
+        ylabel='Volume of Posts')
+
+    return components(fig, INLINE)
 
 @app.route("/")
 def plot_data():
@@ -62,26 +89,9 @@ def plot_data():
     volume of posts versus neighborhood
     """
     (df, df_volume, df_by_location) = get_data()
-    dates = df_volume.index.map(lambda x: datetime.strptime(x, '%Y-%m-%d'))
+    script1, div1 = volumeGraph(df_volume)
+    script2, div2 = locationGraph(df_by_location)
 
-    TOOLS = "pan,wheel_zoom,box_zoom,reset,resize"
-    fig1 = figure(tools=TOOLS, 
-            title='Posts Volume',
-            x_axis_label='Date',
-            y_axis_label='Volume of Posts',
-            x_axis_type='datetime')
-    fig1.line(dates, df_volume.values.tolist(), line_width=3)
-    script1, div1 = components(fig1, INLINE)
-
-    MAX_NEIGHBORHOODS = 20
-    data = {"y": df_by_location.values.tolist()[:MAX_NEIGHBORHOODS]}
-    fig2 = Bar(data, 
-        cat=df_by_location.index.values.tolist()[:MAX_NEIGHBORHOODS], 
-        title="Post Volume per Neighborhood",
-        xlabel='Neighborhood', 
-        ylabel='Volume of Posts')
-
-    script2, div2 = components(fig2, INLINE)
     plot_resources = RESOURCES.render(
         js_raw=INLINE.js_raw,
         css_raw=INLINE.css_raw,
